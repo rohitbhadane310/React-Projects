139ebaa33818626df6f3a536e7a60e7e5ed75126
Rohit Bhadane <rohitbhadane310@gmail.com>
2022-12-29 21:28:23 +0530

    complete App

--- Todo App using react/todo-app/src/components/Todo.js
+++ Todo App using react/todo-app/src/components/Todo.js
@@ -1,30 +1,64 @@
-import React from "react"
-import './Todo.css'
+import React, { useState } from "react";
+import ShowTodo from "./ShowTodo";
+import "./Todo.css";
+
+function Todo() {
+  const [task, setTask] = useState("add some task");
+  const [data, setData] = useState([]);
+
+  const onChangeHandler = (element) => {
+    setTask(element.target.value);
+  };
+
+  const submitHandler = (element) => {
+    element.preventDefault();
+    const newData = task;
+    setData([...data, newData]);
+
+    setTask("");
+  };
 
 
-function Todo(){
-    return (
-        <div className="container">
-            <div className="row justify-content-center align-items-center main-row">
-                <div className="col shadow main-col bg-white">
-                    <div className="row bg-primary text-white">
-                        <div className="col  p-2">
-                            <h4 className='text-center'>Todo App Using React JS</h4>
-                        </div>
-                    </div>
-                    <form>
-                        <div className="row justify-content-between text-white p-2">
-                            <div className="form-group flex-fill mb-2 col-9">
-                                <input id="todo-input" type="text" className="form-control" />
-                            </div>
-                            <button type="submit" className="btn btn-primary mb-2 ml-2 col-3">Add todo</button>
-                        </div>
-                    </form>
-    
-                </div>
+  const deleteItem = (ele) =>{
+    const finalData = data.filter((curEle , index)=>{
+        return index !== ele;
+    })
+    setData(finalData)
+  }
+
+  return (
+    <div className="container">
+      <div className="row justify-content-center align-items-center main-row">
+        <div className="col shadow main-col bg-white">
+          <div className="row bg-primary text-white">
+            <div className="col  p-2">
+              <h4 className="text-center">Todo App Using React JS</h4>
             </div>
+          </div>
+          <form onSubmit={submitHandler}>
+            <div className="row justify-content-between text-white p-2">
+              <div className="form-group flex-fill mb-2 col-9">
+                <input
+                  id="todo-input"
+                  type="text"
+                  className="form-control"
+                  value={task}
+                  onChange={onChangeHandler}
+                />
+              </div>
+              <button type="submit" className="btn btn-primary mb-2 ml-2 col-3">
+                Add todo
+              </button>
+            </div>
+          </form>
+
+          {data.map((value, index) => {
+            return<ShowTodo key={index} id={index} task={value} onSelect={deleteItem}/>;
+          })}
         </div>
-    )    
+      </div>
+    </div>
+  );
 }
 
 export default Todo;
