74289e35319f6c9407b7745f222876bf4f43c20e
Rohit Bhadane <rohitbhadane310@gmail.com>
2023-02-01 22:21:24 +0530

    using btn in app

--- clock/clock-app/src/App.js
+++ clock/clock-app/src/App.js
@@ -1,13 +1,23 @@
 
-import React from 'react';
+import React, { useState } from 'react';
+import './App.css';
 
 const App = () =>{
 
   let time = new Date().toLocaleTimeString();
 
+  const [currTime , setcurrTime] = useState(time);
+
+  const updateTime = () =>{
+    let time = new Date().toLocaleTimeString();
+    setcurrTime(time);
+  }
+
+
   return(
     <>
-    <h1>{time}</h1>
+    <h1 className='App'>{currTime}</h1>
+    <button className='btn' onClick={updateTime}>get Time</button>
     </>
   )
 }
