9ee2716d2842f9eae5af8ed6276ac2a6d9efac24
Rohit Bhadane <rohitbhadane310@gmail.com>
2023-02-07 20:18:06 +0530

    use the hook for display

--- simple calculator/calci/src/App.js
+++ simple calculator/calci/src/App.js
@@ -1,10 +1,38 @@
-import React from 'react';
-import Layout from './components/Layout.js';
+import React, { useState } from 'react';
 import './App.css';
 
 const App = () => {
+
+  const [result , setResult] = useState("");
+
+  const clickHandle = (event) =>{
+
+    setResult(result.concat(event.target.value))
+  }
+
   return (
-    <Layout/>
+    <div className='calc'>
+    <input type="text" placeholder='0' id='answer' value={result}/>
+    <input type="button" value="9" className='button' onClick={clickHandle}/>
+    <input type="button" value="8" className='button' onClick={clickHandle}/>
+    <input type="button" value="7" className='button' onClick={clickHandle}/>
+    <input type="button" value="6" className='button' onClick={clickHandle}/>
+    <input type="button" value="5" className='button' onClick={clickHandle}/>
+    <input type="button" value="4" className='button' onClick={clickHandle}/>
+    <input type="button" value="3" className='button' onClick={clickHandle}/>
+    <input type="button" value="2" className='button' onClick={clickHandle}/>
+    <input type="button" value="1" className='button' onClick={clickHandle}/>
+    <input type="button" value="0" className='button' onClick={clickHandle}/>
+    <input type="button" value="." className='button' onClick={clickHandle}/>
+    <input type="button" value="+" className='button' onClick={clickHandle}/>
+    <input type="button" value="-" className='button' onClick={clickHandle}/>
+    <input type="button" value="*" className='button' onClick={clickHandle}/>
+    <input type="button" value="/" className='button' onClick={clickHandle}/>
+    <input type="button" value="%" className='button' onClick={clickHandle}/>
+    <input type="button" value="Clear" className='button button1'/>
+    <input type="button" value="=" className='button button1'/>
+    
+  </div>
   );
 };
 
