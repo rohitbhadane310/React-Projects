24ed42bc1f7c972ba27571adc716ab2ece234b3b
Rohit Bhadane <rohitbhadane310@gmail.com>
2023-02-28 11:34:05 +0530

    half project is complete

--- image gallery/image-gallery/src/components/PageCom.js
+++ image gallery/image-gallery/src/components/PageCom.js
@@ -1,18 +1,54 @@
-
-import React, { useState } from 'react'
+import React, { useState } from "react";
+import backgroundImage from "../image/bg.jpg";
 
 function PageCom() {
+  const [value, setValue] = useState("");
 
-  const [value , setValue] = useState("");
+  const [results, setResults] = useState([]);
+
+  const fetchImages = () => {
+    fetch(
+      `https://api.unsplash.com/search/photos/?client_id=ZnHSRY-UGzrH5Dk-blCZZuWTFuakB6svvU7QsU9NfK4&query=${value}&orientation=squarish`
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        // console.log(data)
+        setResults(data.results);
+      });
+  };
   return (
-    <div className='App'>
-        <div className='mydiv'>
-            <span>Search:</span>
-            <input type="text" value={value} onChange={(e) => setValue(e.target.value)}/>
-            <button>send</button>
-        </div>
+    <div className="App">
+      <div
+        className="mydiv"
+        style={{
+          backgroundImage: "url(" + backgroundImage + ")",
+          backgroundSize: "cover",
+          height: "50vh",
+          color: "#f5f5f5",
+        }}
+      >
+        {/* <span>Search:</span> */}
+        <input
+          type="text"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <button className="btn btn-success" onClick={() => fetchImages()}>search</button>
+      </div>
+      <div className="gallery">
+        {results.map((item) => {
+          return (
+            <img
+              className="item"
+              key={item.id}
+              src={item.urls.regular}
+              alt="images"
+            />
+          );
+        })}
+      </div>
     </div>
-  )
+  );
 }
 
 export default PageCom;
