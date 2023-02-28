7460071b6f049b99b7ac1d49af22eeb3b1c27fcd
Rohit Bhadane <rohitbhadane310@gmail.com>
2023-02-28 18:24:29 +0530

    completed the project

--- image gallery/image-gallery/src/App.js
+++ image gallery/image-gallery/src/App.js
@@ -1,14 +1,30 @@
 
+import React , {useState} from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import TopNav from './components/TopNav';
 // import pageImage from './image/bg.jpg';
 import PageCom from './components/PageCom';
+import DarkMode from './components/DarkMode';
 
 function App() {
+
+  const [mode, setMode] = useState('light');
+
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = 'black';
+    }
+    else {
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+    }
+  }
+
   return (
     <>
-    <TopNav/>
+    <TopNav title="ImageGallery" mode={mode} toggleMode={toggleMode}/>
     {/* <img src={pageImage} alt = "image"/> */}
     <PageCom/>
     </>
