a434d2d13268836060bfadf1d9c93b66685ccffd
Rohit Bhadane <rohitbhadane310@gmail.com>
2023-03-23 18:27:34 +0530

    calci setup

--- calculator/calci-app/src/components/Layout.js
+++ calculator/calci-app/src/components/Layout.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useReducer, useState } from "react";
 import "../App.css";
 
 function Layout() {
+
+  const [val , setCurrval] = useState(12);
+
+  const changeval = () =>{
+    setCurrval = 34;
+  }
+
   return (
     <div className="calculator">
-      <div className="calculator-display"></div>
+      <div className="calculator-display">
+        <div className="pre-output">{changeval}</div>
+        <div className="curr-output">{val}</div>
+      </div>
       <div className="calculator-keypad">
         <div className="calculator-keypad-row">
           <button className="calculator-key">7</button>
