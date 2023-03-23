a0352f8dcd04aa1d3a7f1559ea4312a1341aaa01
Rohit Bhadane <rohitbhadane310@gmail.com>
2023-03-23 19:04:18 +0530

    jsx layout created

--- calci/calci-app/src/App.js
+++ calci/calci-app/src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import './App.css'
 
 function App() {
+
+  const [result  , setResult] = useState("")
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className="container">
+         <form >
+             <input type="text" value={result} />
+         </form>
+
+         <div className="keyboard">
+                <button>clear</button>
+                <button>c</button>
+                <button>&divide;</button>
+                <button>7</button>
+                <button>8</button>
+                <button>9</button>
+                <button>&times;</button>
+                <button>4</button>
+                <button>5</button>
+                <button>6</button>
+                <button>&ndash;</button>
+                <button>1</button>
+                <button>2</button>
+                <button>3</button>
+                <button>+</button>
+                <button>0</button>
+                <button>.</button>
+                <button>=</button>
+         </div>
     </div>
-  );
+    </>
+  )
 }
 
-export default App;
+export default App
